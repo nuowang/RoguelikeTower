d136f8335f46bb445b4e2fd7f298bea721e197c0
nuowang <wolich22@gmail.com>
2017-12-10 16:26:01 -0800

    work toward square

--- app/src/main/java/com/nwp/rogueliketower/RoguelikeTower.java
+++ app/src/main/java/com/nwp/rogueliketower/RoguelikeTower.java
@@ -19,56 +19,64 @@
 package com.nwp.rogueliketower;
 
 import com.nwp.lib.gameplay.Game;
-import com.nwp.lib.gles.MyGLSurfaceView;
+import com.nwp.lib.gles.MyGLRenderer;
 
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
 public class RoguelikeTower extends Game {
 
-    public GLSurfaceView mGLView;
+    /** Hold a reference to our GLSurfaceView */
+    private GLSurfaceView mGLSurfaceView;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
 
-        // For android library imaging rendering
-        setContentView(R.layout.main);
+        mGLSurfaceView = new GLSurfaceView(this);
 
-        // Create a GLSurfaceView instance and set it as the ContentView for this Activity.
-        mGLView = new MyGLSurfaceView(this);
-        setContentView(mGLView);
+        // Check if the system supports OpenGL ES 2.0.
+        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
 
+        if (supportsEs2)
+        {
+            // Request an OpenGL ES 2.0 compatible context.
+            mGLSurfaceView.setEGLContextClientVersion(2);
+
+            // Set the renderer to our demo renderer, defined below.
+            mGLSurfaceView.setRenderer(new MyGLRenderer());
+        }
+        else
+        {
+            // This is where you could create an OpenGL ES 1.x compatible
+            // renderer if you wanted to support both ES 1 and ES 2.
+            return;
+        }
+
+        setContentView(mGLSurfaceView);
     }
 
-//    @Override
-//    public void onRestart() {
-//        super.onRestart();
-//    }
-//
-//    @Override
-//    public void onStart() {
-//        super.onStart();
-//    }
-//
-//    @Override
-//    public void onResume(){
-//        super.onResume();
-//    }
-//
-//    @Override
-//    public void onPause() {
-//        super.onPause();
-//    }
-//
-//    @Override
-//    public void onStop() {
-//        super.onStop();
-//    }
-//
-//    @Override
-//    public void onDestroy() {
-//        super.onDestroy();
-//    }
+    @Override
+    public void onResume()
+    {
+        // The activity must call the GL surface view's onResume() on activity onResume().
+        super.onResume();
+        mGLSurfaceView.onResume();
+    }
+
+    @Override
+    public void onPause()
+    {
+        // The activity must call the GL surface view's onPause() on activity onPause().
+        super.onPause();
+        mGLSurfaceView.onPause();
+    }
 
 }
